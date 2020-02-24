41fdac6a759e45fdba236bb0ff6df953297af564
kkuop <60794860+kkuop@users.noreply.github.com>
2020-02-24 10:18:20 -0600

    preparing for TDD by adding UnitTest project to solution

---
+++ UnitTests/CustomListTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class CustomListTests
+    {
+        //Add tests
+        [TestMethod]
+        public void AddToList()
+        {
+        }
+
+        //Remove tests
+
+        //Count tests
+
+        //Capacity tests
+
+        //Indexer tests
+
+        //ToString tests
+
+        //Overload add tests
+
+        //Overload minus tests
+
+        //Zipper tests
+
+        //Iterate tests
+
+        //BONUS sort tests
+    }
+}
