5de48f711fb3c9de5c06e044e96bfc746f71b3cd
kkuop <60794860+kkuop@users.noreply.github.com>
2020-02-24 11:15:22 -0600

    adding method for indexer and skeleton for other methods

--- CustomList/CustomList.cs
+++ CustomList/CustomList.cs
@@ -4,17 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CustomList
+namespace CustomListProject
 {
-    public class CustomList
+    public class CustomList<T>
     {
         //member vars
-
+        private T[] arrayBackbone = new T[10];
+        public int Count { get; }
+        public int Capacity { get; }
         //constructor
         public CustomList()
         {
 
         }
         //member methods
+        public void Add(T item)
+        {
+
+        }
+        public void Remove(T item)
+        {
+
+        }
+        public override string ToString()
+        {
+            return "";
+        }
+        public List<T> Zip(List<T> list, List<T> list1)
+        {
+            List<T> newList = new List<T>();
+            return newList;
+        }
+        public List<T> Sort(List<T> list)
+        {
+            return list;
+        }
+        public T this[int i]
+        {
+            get { return arrayBackbone[i]; }
+            set { arrayBackbone[i] = value; }
+        }
+        public readonly struct Plus
+        {
+
+        }
+        public readonly struct Minus
+        {
+
+        }
     }
 }
--- UnitTests/CustomListTests.cs
+++ UnitTests/CustomListTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomListProject;
 
 namespace UnitTests
 {
@@ -10,36 +11,79 @@ namespace UnitTests
         [TestMethod]
         public void AddIntsToList()
         {
-
+            //arrange
+            CustomList<int> customList = new CustomList<int>();
+            int expected = 1;
+            int actual;
+            //act
+            customList.Add(1);
+            actual = customList[0];
+            //assert
+            Assert.AreEqual(expected, actual);
         }
         public void AddStringsToList()
         {
+            //arrange
+            CustomList<string> customList = new CustomList<string>();
+            //act
+
+            //assert
 
         }
         public void AddObjectsToList()
         {
+            //arrange
+            CustomList<Random> customList = new CustomList<Random>(); 
+            //act
+
+            //assert
 
         }
         public void AddDoublesToList()
         {
+            //arrange
+            CustomList<double> customList = new CustomList<double>();
+            //act
+
+            //assert
 
         }
 
         //Remove tests
         public void RemoveIntsFromList()
         {
+            //arrange
+
+            //act
+
+            //assert
 
         }
         public void RemoveStringFromList()
         {
+            //arrange
+
+            //act
+
+            //assert
 
         }
         public void RemoveObjectFromList()
         {
+            //arrange
+
+            //act
+
+            //assert
 
         }
         public void RemoveDoubleFromList()
         {
+            //arrange
+
+            //act
+
+            //assert
 
         }
 
